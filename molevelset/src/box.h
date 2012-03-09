ee69b6ba1611de2c0a4201c4a82284b056165271
Leif Johnson <leif.t.johnson@gmail.com>
2012-03-08 21:58:51 -0800

    cleanup.

--- molevelset/src/box.h
+++ molevelset/src/box.h
@@ -1,23 +1,4 @@
 #ifndef BOX_H
 #define BOX_H
 
-typedef struct box {
-  int nbox;
-  int arr_max;
-  int **splits;
-  double *Y;
-  int *id;
-  double cost;
-  int inset;
-  struct box *left, *right;
-  int *checked;
-} box;
-
-box *newbox(int dim);
-void deletebox(box *);
-void addtobox(box *, double Y, int id);
-
-int make_key(void *); // make integer key out of box description
-int cmp_keys(void *, void *); // return 0 if keys are equal, non zero otherwise
-
 #endif
