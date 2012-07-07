1a0de0b160b43cfd22981723359aaef76d092128
Leif Johnson <leif.t.johnson@gmail.com>
2012-07-06 23:54:57 -0700

    Stubbed out call to C++ code.

--- molevelset/src/rmain.cpp
+++ molevelset/src/rmain.cpp
@@ -20,12 +20,14 @@ SEXP molevelset(SEXP x, SEXP y, SEXP kmax, SEXP gamma, SEXP rho, SEXP delta) {
   PROTECT(nSexp = allocVector(INTSXP, 1));
   INTEGER(nSexp)[0] = n;
   PROTECT(dimSexp = allocVector(INTSXP, 1));
-  INTEGER(dimSexp)[0] = dimSexp;
+  INTEGER(dimSexp)[0] = dim;
   
   SET_VECTOR_ELT(result, 0, nSexp);
   SET_VECTOR_ELT(result, 1, dimSexp);
+
   SET_STRING_ELT(resultNames, 0, mkChar("n"));
   SET_STRING_ELT(resultNames, 1, mkChar("dim"));
+  namesgets(result, resultNames);
   
   UNPROTECT(4);
   
