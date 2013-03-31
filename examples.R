fe3c66279659863d3a52c92895032fbd94c92d4f
Leif Johnson <leif.t.johnson@gmail.com>
2013-03-30 23:38:44 -0700

    Clean up plotting.

--- examples.R
+++ examples.R
@@ -9,26 +9,41 @@ X <- as.matrix(X)
 Y <- ifelse(rowSums((X - 1)^2) <= (0.2 * sqrt(2))^2, 1, 0)
 
 le <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=0)
-plot.molevelset(le, points=FALSE)
+plot.molevelset(le, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
 
 le1 <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=1)
-plot.molevelset(le1, points=FALSE)
+plot.molevelset(le1, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
+
 
 le100 <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=100)
-plot.molevelset(le100, points=FALSE)
+plot.molevelset(le100, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
 
 le1000 <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=1000)
-plot.molevelset(le1000, points=FALSE)
+plot.molevelset(le1000, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
 
 le5000 <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=5000)
-plot.molevelset(le5000, points=FALSE)
+plot.molevelset(le5000, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
 
 le7500 <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=7500)
-plot.molevelset(le7500, points=FALSE)
+plot.molevelset(le7500, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
+
 
 le10000 <- molevelset(X, Y, k.max=k.max, gamma=0.5, rho=10000)
-plot.molevelset(le10000, points=FALSE)
-
+plot.molevelset(le1000, points=FALSE, combine.boxes=TRUE,
+                col.noninset=NA, border.noninset=NA,
+                col.inset="black", border.inset=NA)
 
 ###########################################################################
 ## Diagonal split
@@ -40,7 +55,7 @@ gamma <- 0.5
 plot(X, col=ifelse(Y >= gamma, "red", "black"))
 
 le <- molevelset(X, Y, k.max=5, gamma=0.5, rho=0)
-plot.molevelset(le)
+plot.molevelset(le, combine.boxes=TRUE)
 abline(0, 1, lwd=3, lty=2)
 
 le <- molevelset(X, Y, k.max=5, gamma=0.5, rho=0.05)
@@ -50,3 +65,22 @@ abline(0, 1, lwd=3, lty=2)
 le <- molevelset(X, Y, k.max=5, gamma=0.5, rho=1.00)
 plot.molevelset(le)
 abline(0, 1, lwd=3, lty=2)
+
+# Same example, but a regular collection of points.
+X <- expand.grid(seq(0, 1, by=2**-5), seq(0, 1, by=2**-5))
+X <- unname(as.matrix(X))
+Y <- 1 * (X[, 1] < X[, 2])
+
+le <- molevelset(X, Y, k.max=3, gamma=0.5, rho=0)
+plot.molevelset(le, combine.boxes=TRUE, which="inset")
+abline(0, 1, lwd=3, lty=2)
+
+le <- molevelset(X, Y, k.max=4, gamma=0.5, rho=0)
+plot.molevelset(le, combine.boxes=TRUE)
+abline(0, 1, lwd=3, lty=2)
+
+le <- molevelset(X, Y, k.max=5, gamma=0.5, rho=0.10)
+plot.molevelset(le)
+abline(0, 1, lwd=3, lty=2)
+
+plot.molevelset(le, combine.boxes=TRUE)
--- molevelset/R/graphics.R
+++ molevelset/R/graphics.R
@@ -1,15 +1,10 @@
-get.levelset.polygons <- function(le.boxes, k.max, combine.edges=FALSE) {
+get.levelset.polygons <- function(le.boxes) {
   # Get polygons representing a levelset.
   #
   # Args:
   #   le.boxes: list of matricies of leveset boxes.
-  #   k.max: numeric, maximum number of splits.
-  #   combine.edges: logical, should the boxes be simplified.
   # Returns:
   #   List of matricies containing polygons verticies
-  if (combine.edges) {
-    waring("Combining edges is not implemented.")
-  }
   boxes <- lapply(le.boxes, "[[", "box")
   polygons <- lapply(boxes,
                      function(box) cbind(box[c(1, 1, 2, 2), 1],
@@ -17,16 +12,50 @@ get.levelset.polygons <- function(le.boxes, k.max, combine.edges=FALSE) {
   return(polygons)                                       
 }
 
+get.levelset.lines <- function(le.boxes, k.max) {
+  step <- 2**-k.max
+  x.edges <- matrix(0, nrow=2**k.max, ncol=2**k.max + 1)
+  y.edges <- matrix(0, nrow=2**k.max + 1, ncol=2**k.max)
+  boxes <- lapply(le.boxes, "[[", "box")
+  for (i in seq_along(boxes)) {
+    # Increment the edge count for edge edge in this box.
+    box <- boxes[[i]] / step + 1
+    x.seq <- seq(box[1, 1], box[2, 1] - 1) 
+    y.seq <- seq(box[1, 2], box[2, 2] - 1)
+    x.edges[x.seq, box[, 2]] <- x.edges[x.seq, box[, 2]] + 1
+    y.edges[box[, 1], y.seq] <- y.edges[box[, 1], y.seq] + 1
+  }
+
+  # Edge locations with count == 1 should be plotted because they are on
+  # exactly one box border.
+  x.edges <- which(x.edges == 1, arr.ind=TRUE)
+  y.edges <- which(y.edges == 1, arr.ind=TRUE)
+
+  # Extract the lines.
+  f.main <- function(e) rbind(e - 1, e) * step
+  f.secondary <- function(e) (rbind(e, e) - 1) * step
+  
+  x.x <- f.main(x.edges[, 1])
+  x.y <- f.secondary(x.edges[, 2])
+  x.lines <- lapply(seq_len(ncol(x.x)), function(i) cbind(x.x[, i], x.y[, i]))
+  
+  y.x <- f.secondary(y.edges[, 1])
+  y.y <- f.main(y.edges[, 2])
+  y.lines <- lapply(seq_len(ncol(y.y)), function(i) cbind(y.x[, i], y.y[, i]))
+
+  return(c(x.lines, y.lines))
+}
+
 .is.color.specification <- function(col) {
   return(!is.null(col) && !is.na(col) && is.character(col) && col !="")
 }
 
-.plot.polygons <- function(polys, border=NULL, col=NA) {
+.plot.polygons <- function(polys, border=NA, col=NA) {
   if (!.is.color.specification(col)) {
     col <- NA
   }
   if (!.is.color.specification(border)) {
-    border <- NULL
+    border <- NA
   }
   for (i in seq_along(polys)) {
     polygon(polys[[i]], border=border, col=col)
@@ -34,11 +63,12 @@ get.levelset.polygons <- function(le.boxes, k.max, combine.edges=FALSE) {
 }
 
 plot.molevelset <- function(le,
+                            which=c("both", "inset", "noninset", "neither"),
                             col.inset="gray",
                             col.noninset="gray55",
                             border.inset="red",
                             border.noninset="black",
-                            combine.edges=FALSE,
+                            combine.boxes=TRUE,
                             points=TRUE,
                             ...) {
   # Plot a molevelset estimate.
@@ -57,20 +87,45 @@ plot.molevelset <- function(le,
   #   ...: additional args passed to plot.
   # Returns:
   #   Nothing.
-
+  which <- match.arg(which)
+  
   # Can we let the user specifiy xlab etc if they want to without messing up
   # our empty default?  Possibly by looking for xlab in ... or adding xlab=""
   # to ... if xlab is not allrady in there.
   plot(c(0, 1), c(0, 1), type="n", xlab="", ylab="", ...)
+  
+  plot.inset <- which %in% c("both", "inset")
+  plot.noninset <- which %in% c("both", "noninset")
 
-  .plot.polygons(get.levelset.polygons(le$inset_boxes,
-                                       combine.edges=combine.edges),
-                 border=border.inset,
-                 col=col.inset)
-  .plot.polygons(get.levelset.polygons(le$non_inset_boxes,
-                                       combine.edges=combine.edges),
-                 border=border.noninset,
-                 col=col.noninset)
+  if (!combine.boxes) {
+    if (plot.inset)
+      .plot.polygons(get.levelset.polygons(le$inset_boxes),
+                     border=border.inset,
+                     col=col.inset)
+    if (plot.noninset) 
+      .plot.polygons(get.levelset.polygons(le$non_inset_boxes),
+                     border=border.noninset,
+                     col=col.noninset)
+  } else {
+    if (plot.inset && .is.color.specification(col.inset)) 
+      .plot.polygons(get.levelset.polygons(le$inset_boxes),
+                     border=NA,
+                     col=col.inset)
+    if (plot.noninset && .is.color.specification(col.noninset))
+      .plot.polygons(get.levelset.polygons(le$non_inset_boxes),
+                     border=NA,
+                     col=col.noninset)
+    if (plot.inset && .is.color.specification(border.inset)) {
+      inset.lines <- get.levelset.lines(le$inset_boxes, le$k.max)
+      for (i in seq_along(inset.lines))
+        lines(inset.lines[[i]], col=border.inset)
+    }
+    if (plot.noninset && .is.color.specification(border.noninset)) {
+      non.inset.lines <- get.levelset.lines(le$non_inset_boxes, le$k.max)
+      for (i in seq_along(non.inset.lines))
+        lines(non.inset.lines[[i]], col=border.noninset)
+    }
+  }
   if (points)
     graphics::points(le$X, col=ifelse(le$Y >= le$gamma, "red", "black"))
 }
